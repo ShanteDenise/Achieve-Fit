8f6f920ecade3246134d92cc7686edff35bddb1e
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-19 17:47:13 -0500

    imported Register and Login to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React, { Component ***REMOVED*** from 'react';
 import { BrowserRouter as Router, Route, Switch ***REMOVED*** from 'react-router-dom'
 import Navbar from './component/layout/Navbar';
 import Landing from './component/layout/Landing';
+import Register from './component/auth/Register';
+import Login from './component/auth/Login';
+
+
 import './App.css';
 
 class App extends Component {
@@ -13,6 +17,9 @@ class App extends Component {
          <Navbar />
             <Route exact path="/" component={ Landing ***REMOVED***/>
              <div className="container">
+             <Route exact path="/register" component={ Register ***REMOVED*** />
+             <Route exact path="/login" component={ Login ***REMOVED*** />
+
             </div>
       </div>
     
---
+++ client/src/component/auth/Register.js
@@ -0,0 +1,14 @@
+import React, { Component ***REMOVED*** from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                
+            </div>
+        );
+    ***REMOVED***
+***REMOVED***
+
+export default Register;
