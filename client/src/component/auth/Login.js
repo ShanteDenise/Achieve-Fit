73cbc9a78e64ded77e1f44ea23c32cddbcc06cc4
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-19 18:53:29 -0500

    imported link from react-router

--- client/src/component/auth/Login.js
+++ client/src/component/auth/Login.js
@@ -1,115 +1,14 @@
 import React, { Component ***REMOVED*** from 'react';
-import PropTypes from 'prop-types';
-import { connect ***REMOVED*** from 'react-redux';
-import classnames from 'classnames';
-import { loginUser ***REMOVED*** from '../../actions/authActions';
 
 class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      password: '',
-      errors: {***REMOVED***
-    ***REMOVED***;
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  ***REMOVED***
-
-  componentDidMount() {
-    if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
-    ***REMOVED***
-  ***REMOVED***
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
-    ***REMOVED***
-
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors ***REMOVED***);
-    ***REMOVED***
-  ***REMOVED***
-
-  onSubmit(e) {
-    e.preventDefault();
-
-    const userData = {
-      email: this.state.email,
-      password: this.state.password
-    ***REMOVED***;
-
-    this.props.loginUser(userData);
-  ***REMOVED***
-
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value ***REMOVED***);
-  ***REMOVED***
-
-  render() {
-    const { errors ***REMOVED*** = this.state;
-
-    return (
-      <div className="login">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">
-                Sign in to your DevConnector account
-              </p>
-              <form onSubmit={this.onSubmit***REMOVED***>
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className={classnames('form-control form-control-lg', {
-                      'is-invalid': errors.email
-                    ***REMOVED***)***REMOVED***
-                    placeholder="Email Address"
-                    name="email"
-                    value={this.state.email***REMOVED***
-                    onChange={this.onChange***REMOVED***
-                  />
-                  {errors.email && (
-                    <div className="invalid-feedback">{errors.email***REMOVED***</div>
-                  )***REMOVED***
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    className={classnames('form-control form-control-lg', {
-                      'is-invalid': errors.password
-                    ***REMOVED***)***REMOVED***
-                    placeholder="Password"
-                    name="password"
-                    value={this.state.password***REMOVED***
-                    onChange={this.onChange***REMOVED***
-                  />
-                  {errors.password && (
-                    <div className="invalid-feedback">{errors.password***REMOVED***</div>
-                  )***REMOVED***
-                </div>
-                <input type="submit" className="btn btn-info btn-block mt-4" />
-              </form>
+    render() {
+        return (
+            <div>
+                <h1>Login Page</h1>
+                
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  ***REMOVED***
+        );
+    ***REMOVED***
 ***REMOVED***
 
-Login.propTypes = {
-  loginUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
-***REMOVED***;
-
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors
-***REMOVED***);
-
-export default connect(mapStateToProps, { loginUser ***REMOVED***)(Login);
+export default Login;
--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -4,7 +4,7 @@ class Register extends Component {
     render() {
         return (
             <div>
-                <h1>Login</h1>
+                <h1>Register</h1>
                 
             </div>
         );
