cf5dfa0e3010bc114a398bbe147b8b3112692230
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-11-19 14:52:21 -0500

    Created exact rout to landing page

--- client/src/component/layout/Landing.js
+++ client/src/component/layout/Landing.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="landing">
+            <div className="dark-overlay landing-inner text-light">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    <h1 className="display-3 mb-4">Developer
+                    </h1>
+                    <p className="lead"> Create a Fitness profile/portfolio, share posts and get help from other Fitness Freaks</p>
+                    <hr />
+                    <Link className="btn btn-lg btn-info mr-2" to="/register">Sign Up</Link>
+                    <Link className="btn btn-lg btn-light" to="/login">Login</Link>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
+         
         );
     }
 }
