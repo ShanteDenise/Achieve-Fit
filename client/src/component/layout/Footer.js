7a6858b7899013b3c30f3ae98a0882b8e23d150f
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-11-19 15:36:43 -0500

    Added footer to layout footer

--- client/src/component/layout/Footer.js
+++ client/src/component/layout/Footer.js
@@ -4,7 +4,9 @@ class Footer extends Component {
     render() {
         return (
             <div>
-                
+                <footer className="bg-dark text-white mt-5 p-4 text-center">
+                Copyright &copy; {new Date().getFullYear()} Achieve-Fit
+                </footer> 
             </div>
         );
     }
