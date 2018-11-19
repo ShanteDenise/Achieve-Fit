1e86af0b137f8f955b811618eda25a2e5f666ce5
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-19 17:41:41 -0500

    Removed footer from layout, decided I did not need it

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './component/layout/Navbar';
-import Landing from './component/layout/Landing'
+import Landing from './component/layout/Landing';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
       <Router>
-        <Switch>
-      <Navbar/>
-      <Route exact path="/" component={ Landing }/>
+      <Switch>
+      <div className="App">
+         <Navbar />
+            <Route exact path="/" component={ Landing }/>
+             <div className="container">
+            </div>
+      </div>
+    
       </Switch>
       </Router>
-      </div>
+     
     );
   }
 }
