e212e536fb8c02a98a555b55daa0c04c8629157c
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-19 01:22:19 -0500

    Added server.js from other working server file

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+// const users = require('./routes/api/users')
+// const posts = require('./routes/api/post')
+// const profile = require('./routes/api/profile')
+const passport = require('passport')
+
+const app = express();
+
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended:false***REMOVED***));
+app.use(bodyParser.json());
+
+// const db = require('./config/key').mongoURI;
+
+//Connect to mongodb
+mongoose.connect(db).then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err))
+
+//Passport Middlewear
+app.use(passport.initialize());
+
+//passport config
+// require('./config/passport')(passport);
+
+app.get('/', (req, res) => console.log('Hello'))
+// app.use('/api/users', users);
+// app.use('/api/profile', profile);
+// app.use('/api/posts', posts);
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(`Server running on port ${port***REMOVED***`));
