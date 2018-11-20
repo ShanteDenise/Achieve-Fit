3213837897a87094202e7405abcc5c41d2129e60
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-11-20 10:55:14 -0500

    Added a folder for validation, using the same function from backend

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,6 @@
 import { SET_CURRENT_USER } from '../actions/types'
+import { SERVFAIL } from 'dns';
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
 
 
 const initialSate = {
@@ -8,6 +10,12 @@ const initialSate = {
 
 export default function(state = initialSate, action){
     switch(action.type){
+        case SET_CURRENT_USER:
+        return {
+            ...state,
+            isAuthenticated: !isEmpty(action.payload),
+            user: action.payload
+        }
         default:
         return state;
     }
