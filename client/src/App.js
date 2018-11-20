d1fe2b218d51b7289d31c7dc47ca8ed1617cf024
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-20 03:03:34 -0500

    Added Provider for store in redux and wrapped Provider around everything in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component ***REMOVED*** from 'react';
-import { BrowserRouter as Router, Route, Switch ***REMOVED*** from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch ***REMOVED*** from 'react-router-dom';
+import { Provider ***REMOVED*** from 'react-redux'
 import Navbar from './component/layout/Navbar';
 import Landing from './component/layout/Landing';
 import Register from './component/auth/Register';
@@ -11,6 +12,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <Provider store ={ store ***REMOVED*** >
       <Router>
       <Switch>
       <div className="App">
@@ -25,6 +27,7 @@ class App extends Component {
     
       </Switch>
       </Router>
+      </Provider>
      
     );
   ***REMOVED***
