91c66bf9562668bf61174885a29acfe5b2949603
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-11-20 17:00:55 -0500

    removed unwanted items from my model

--- client/src/component/createProfile/CreateProfile.js
+++ client/src/component/createProfile/CreateProfile.js
@@ -1,6 +1,19 @@
 import React, { Component ***REMOVED*** from 'react';
 
 class CreateProfile extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            handle: '',
+            bio: '',
+            location: '',
+            status: '',
+            goals: '',
+            fitbitusername: '',
+
+
+        ***REMOVED***
+    ***REMOVED***
     render() {
         return (
             <div>
