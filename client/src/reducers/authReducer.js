bcff1cc44dfdc0ca287d3740859d10818ec1e848
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-20 03:17:03 -0500

    setting up boilerplate for redux

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+const initialSate = {
+    isAuthenticated: false,
+    user: {}
+}
+
+export default function(state = initialSate, action){
+    switch(action.type){
+        default:
+        return state;
+    }
+}
