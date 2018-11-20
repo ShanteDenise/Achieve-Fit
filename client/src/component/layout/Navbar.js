4519c190211c83ad19217a09cbc85a8658b0312d
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-11-20 11:40:42 -0500

    Created conditional statement for navbar header

--- client/src/component/layout/Navbar.js
+++ client/src/component/layout/Navbar.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
-
+import { logoutUser } from '../../actions/authActions'
 
 class Navbar extends Component {
+  onLogout(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
     render() {
+      const  { isAuthenticated, user } = this.props.auth;
+
+      const auth  = (
+        <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                   <a href = "" onClick={this.onLogout.bind(this)} 
+                   className="nav-item">
+                   <img src={user.avatar} alt={user.name} style={{width: '25px', marginRight: '5px'}}/>
+                   </a>
+                  
+                  </li>
+                </ul>
+      )
+      const unAuthLinks  = (
+        <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/register">
+                      Sign Up
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                </ul>
+      )
 
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
@@ -44,16 +75,23 @@ class Navbar extends Component {
                     </Link>
                   </li>
                 </ul>
+                {isAuthenticated ? auth : unAuthLinks}
               </div>
             </div>
           </nav>
         );
-      }
-                
-       
-        
+      }   
     
     }
+    Navbar.PropTypes = {
+      logoutUser: PropTypes.func.isRequired,
+      auth: PropTypes.object.isRequired
+    }
+    const mapStateToProps = state => ({
+      auth: state.auth,
+    })
+    
+  
     
 
-export default Navbar;
+export default connect(mapStateToProps, {logoutUser})(Navbar);
