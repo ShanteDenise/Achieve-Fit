894d4967d1a6b8e1e748e9169bfd769e0abe1d2e
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-11-20 17:23:01 -0500

    Changed Navbar Links

--- client/src/component/createProfile/CreateProfile.js
+++ client/src/component/createProfile/CreateProfile.js
@@ -1,4 +1,7 @@
 import React, { Component ***REMOVED*** from 'react';
+import { connect ***REMOVED*** from 'react-redux';
+import PropTypes from 'prop-types';
+import TextFieldGroup from '../TextFieldGroup'
 
 class CreateProfile extends Component {
     constructor(props){
@@ -10,17 +13,40 @@ class CreateProfile extends Component {
             status: '',
             goals: '',
             fitbitusername: '',
-
-
+            instagram: ' ',
+            errors: {***REMOVED***
         ***REMOVED***
     ***REMOVED***
     render() {
         return (
-            <div>
-                <h1>Your Profile</h1>
+            <div className="create-profile">
+                <div className="container">
+                 <div className="row">
+                  <div className="col-md-7 m-auto">
+                  <h1 className="display-4 text-center">Create Your Profile</h1>
+                    <p className="lead text-center">
+                    Input your information!</p>
+                    <small className="d-block pb-3">* = required Field</small>
+                  
+                  
+                  
+                  
+                  </div>    
+                 </div>
+                </div>
             </div>
         );
     ***REMOVED***
 ***REMOVED***
+CreateProfile.propTypes = {
+    profile: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+***REMOVED***
+
+
+const mapStateToProps = state => ({
+    profile: state.profile,
+    errors: state.errors
+***REMOVED***)
 
 export default CreateProfile;
