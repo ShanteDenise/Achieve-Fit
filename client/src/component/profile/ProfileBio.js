9fa7c87197a996da5c392f6c48b4dfeb40a7bdba
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-21 02:05:26 -0500

    Added profile components and sub components

---
+++ client/src/component/profile/ProfileBio.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProfileBio extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Profile Bio</h1>
+                
+            </div>
+        );
+    }
+}
+
+export default ProfileBio;
