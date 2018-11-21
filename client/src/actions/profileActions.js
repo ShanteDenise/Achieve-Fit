91ab55350dc4b0d179a20a83a58c1dd7384172e4
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-20 21:54:04 -0500

    Changed name of forms

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -23,6 +23,19 @@ export const getCurrentProfile = () => dispatch => {
 
 }
 
+//Create Profile
+export const createProfile= (profileData, history) => dispatch => {
+    axios
+        .post('/profile', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch ({
+                type: GET_ERRORS,
+                payload: err.response.data
+            }))
+
+}
+
 export const setProfileLoading = () => {
     return {
         type: PROFILE_LOADING
--- client/src/component/createProfile/CreateProfile.js
+++ client/src/component/createProfile/CreateProfile.js
@@ -30,9 +30,6 @@ class CreateProfile extends Component {
 
     }
    
-    
-
-
     onChange(e){
         this.setState({ [e.target.name]: e.target.value});
     }
@@ -89,7 +86,7 @@ class CreateProfile extends Component {
                     />
                        <InputFieldGroup
                     placeholder="Bio"
-                    name="Bio"
+                    name="bio"
                     value={this.state.bio}
                     onChange={this.onChange}
                     errors={errors.bio}
@@ -98,13 +95,13 @@ class CreateProfile extends Component {
                     
                        <TextFieldGroup
                     placeholder="* Goals"
-                    name="Goals"
+                    name="goals"
                     value={this.state.goals}
                     onChange={this.onChange}
                     errors={errors.goals}
                     info="Use comma separated values (ex: Be Leaner, Walk-up at 5am, Gym 5x A Week)"
                     />
-                    
+
                     <input type="submit" className="btn btn-info btn-block mt-4" />
 
                     </form>
@@ -128,6 +125,7 @@ CreateProfile.propTypes = {
 
 const mapStateToProps = state => ({
     profile: state.profile,
+    //Listen for error state that was brought in from reducer
     errors: state.errors
 })
 
