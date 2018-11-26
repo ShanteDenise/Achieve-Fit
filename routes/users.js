e02ef04cd416120afa5940826a8d8128d30eb639
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@psadmins-air.attlocal.net>
2018-11-25 22:57:06 -0500

    required everythng for user route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
