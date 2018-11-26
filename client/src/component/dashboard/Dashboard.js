f3b2cb59bc74c079350a2a44a996a2834412b2f0
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-11-26 12:12:04 -0500

    test

--- client/src/component/dashboard/Dashboard.js
+++ client/src/component/dashboard/Dashboard.js
@@ -27,8 +27,7 @@ class Dashboard extends Component {
       if (Object.keys(profile).length > 0) {
         dashboardContent = (<div>
           <p className="lead text-muted">Welcome <Link to={`/profile/${profile.handle***REMOVED***`***REMOVED***>{user.name***REMOVED***</Link></p>
-          <p className="lead text-muted">Welcome <Link to={`/profile/${profile.bio***REMOVED***`***REMOVED***>{user.bio***REMOVED***</Link></p>
-
+          <p className="lead text-muted">{profile.bio***REMOVED*** <Link to={`/profile/${profile.bio***REMOVED***`***REMOVED***></Link></p>
           <ProfileEdit/>
           <div style={{ marginBottom: '50px'***REMOVED******REMOVED***/>
           <button onClick={this.onDeleteClick.bind(this)***REMOVED*** className="btn btn-danger">Delete My Profile</button>
