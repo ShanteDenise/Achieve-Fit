b9f7d1a7b3d75a7e48b612a01061f3ce9ef5d6d1
Shante <shante.austin19@gmail.com>
2018-12-28 10:48:07 -0500

    Styling to dashboard

--- client/src/component/dashboard/Dashboard.js
+++ client/src/component/dashboard/Dashboard.js
@@ -1,60 +1,80 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
-import ProfileEdit from './ProfileEdit';
-import loader from '../../load.gif';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
+import ProfileEdit from "./ProfileEdit";
+import loader from "../../load.gif";
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
   }
 
-  onDeleteClick(e){
+  onDeleteClick(e) {
     this.props.deleteAccount();
   }
-
 
   render() {
     const { user } = this.props.auth;
     const { profile, loading } = this.props.profile;
 
     let dashboardContent;
-   //When the dashboard is loading the dashboard content will be set to loading gif
+    //When the dashboard is loading the dashboard content will be set to loading gif
     if (profile === null || loading) {
-      dashboardContent = <img src={loader} style={{width: '200px', margin:'auto', display: 'block'}} alt="loading"/>
+      dashboardContent = (
+        <img
+          src={loader}
+          style={{ width: "200px", margin: "auto", display: "block" }}
+          alt="loading"
+        />
+      );
     } else {
       // Check if logged in user has profile data
       if (Object.keys(profile).length > 0) {
-        dashboardContent = ( <div>
-                    
-          <div className="media">
-          <img className="align-self-center mr-3" src={user.avatar} style={{width: '200px', display: 'block'}}></img>
-          <div className="media-body">
-          <h5 className="mt-0">Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link></h5>
-          <p className="lead text-muted bio-text">{profile.bio} <Link to={`/profile/${profile.bio}`}></Link></p>
-          {' '}
-          <p className="mb-0">Location:{profile.location}</p>
-          <div className="mb=10"></div>
-          <ul className="list-group">
-                {profile.goals.slice(0, 6).map((goals, index) => (
-                  <li key={index} className="list-group-item">
-                    <i className="fa fa-check pr-1" />
-                    {goals}
-                  </li>
-                ))}
-              </ul>
-          </div>
-          <div> <button onClick={this.handleCreateNewIdea}>New Idea</button>
-          </div>
-          </div>
-          <ProfileEdit/>
-          <div style={{ marginBottom: '50px'}}/>
-          <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Profile</button>
-        </div>
-        )
+        dashboardContent = (
+          <div>
+            <div className="media">
+              <img
+                className="align-self-center mr-3"
+                src={user.avatar}
+                style={{ width: "200px", display: "block" }}
+              />
+              <div className="media-body">
+                <h5 className="mt-2 mb-3">
+                  Welcome{" "}
+                  <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+                </h5>
+                <h5>My Bio: </h5>
+                <p className="lead text-muted bio-text">
+                  {profile.bio} />
+                </p>{" "}
+                <h5>My Goals:</h5>
+                <ul className="list-group">
+                  {profile.goals.map((goals, index) => (
+                    <li key={index} className="list-group-item">
+                      <i className="fa fa-check pr-1" />
+                      {goals}
+                    </li>
 
+                  ))}
+                </ul>
+                <h5 className="mt-3">My Status: </h5>
+                <p className="lead bio-text mt-2">
+                  {profile.status} 
+                </p>
+              </div>
+            </div>
+            <ProfileEdit />
+            <div style={{ marginBottom: "50px" }} />
+            <button
+              onClick={this.onDeleteClick.bind(this)}
+              className="btn btn-danger"
+            >
+              Delete My Profile
+            </button>
+          </div>
+        );
       } else {
         // User is logged in but has no profile
         dashboardContent = (
@@ -72,18 +92,22 @@ class Dashboard extends Component {
     //render the dashboard content below
     return (
       <div className="dashboard">
-        <div className="container">
+        <div className="card card-body bg-light mb-3">
           <div className="row">
-            <div className="col-md-12">
-              <h1 className="display-4">Dashboard</h1>
-              {dashboardContent}
+            <div className="col-4 col-md-3 m-auto" />
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <h3 className="display-4 text-center text-danger">
+                    Dashboard
+                  </h3>
+                  {dashboardContent}
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-    
-    
     );
   }
 }
@@ -102,4 +126,7 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile, deleteAccount }
+)(Dashboard);
