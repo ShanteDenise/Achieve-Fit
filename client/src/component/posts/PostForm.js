bd7d668cb24a61e219ceac7c93ad25e55d5bfa81
Shante <shante.austin19@gmail.com>
2019-01-16 15:12:42 -0500

    Added

--- client/src/component/posts/PostForm.js
+++ client/src/component/posts/PostForm.js
@@ -47,7 +47,7 @@ class PostForm extends Component {
     return (
       <div className="post-form mb-3">
         <div className="card card-info">
-          <div className="card-header bg-info text-white">Say Somthing...</div>
+          <div className="card-header bg-info text-white">Make a Post...</div>
           <div className="card-body">
             <form onSubmit={this.onSubmit}>
               <div className="form-group">
